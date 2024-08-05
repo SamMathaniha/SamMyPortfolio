74ffb1a3db1aa0264d6406781c9429282ab3000b
SamMathaniha <mathanihasam@gmail.com>
2024-08-06 01:46:29 +0530

    Added assests

---
+++ assets/js/ButtonImageOpen.js
@@ -0,0 +1,41 @@
+  // JavaScript for Degree results
+  document.getElementById("DegreeImageButton").addEventListener("click", function() {
+    var DegreeImage = document.getElementById("DegreeImage");
+    if (DegreeImage.style.display === "none" || DegreeImage.style.display === "") {
+      DegreeImage.style.display = "block"; // Show the image
+    } else {
+      DegreeImage.style.display = "none"; // Hide the image if already visible
+    }
+  });
+ 
+ // JavaScript for HND results
+ document.getElementById("hndViewResultsButton").addEventListener("click", function() {
+    var hndResultsImage = document.getElementById("hndResultsImage");
+    if (hndResultsImage.style.display === "none" || hndResultsImage.style.display === "") {
+      hndResultsImage.style.display = "block"; // Show the image
+    } else {
+      hndResultsImage.style.display = "none"; // Hide the image if already visible
+    }
+  });
+
+  // JavaScript for Diploma results
+  document.getElementById("diplomaViewResultsButton").addEventListener("click", function() {
+    var diplomaResultsImage = document.getElementById("diplomaResultsImage");
+    if (diplomaResultsImage.style.display === "none" || diplomaResultsImage.style.display === "") {
+      diplomaResultsImage.style.display = "block"; // Show the image
+    } else {
+      diplomaResultsImage.style.display = "none"; // Hide the image if already visible
+    }
+  });
+
+  MERNCertificateImage
+
+    // Mern stack 
+    document.getElementById("MERNCertificateImageButton").addEventListener("click", function() {
+      var MERNCertificateImage = document.getElementById("MERNCertificateImage");
+      if (MERNCertificateImage.style.display === "none" || MERNCertificateImage.style.display === "") {
+        MERNCertificateImage.style.display = "block"; // Show the image
+      } else {
+        MERNCertificateImage.style.display = "none"; // Hide the image if already visible
+      }
+    });
